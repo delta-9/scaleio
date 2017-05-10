027afd61e35a8a485fc0bf02a10a35f891813a35
Vincent <rawpixel.vincent@gmail.com>
2017-05-10 17:22:45 +0700

    update

---
+++ src/predictUI/createLayout.js
@@ -0,0 +1,17 @@
+import { fromJS } from 'immutable';
+
+export default function createLayout(canvas) {
+  // Initiate the layout and store a reference to the engine used.
+  
+  function update(state) {
+  }
+
+
+  function settings(settings) {
+  }
+
+  return {
+    update,
+    settings
+  };
+}
