03868dac8609441542d31a48a94dc4358e0c092c
Vincent <rawpixel.vincent@gmail.com>
2017-05-16 16:37:39 +0700

    remove grommet, add material ui

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import AppWrapper from 'grommet/components/App';
-import Split from 'grommet/components/Split';
-import Sidebar from 'grommet/components/Sidebar';
-import Section from 'grommet/components/Section';
-import Header from 'grommet/components/Header';
 import request from '../../utils/request';
 import createLayout from '../../predictUI/createLayout';
-import Button from 'grommet/components/Button';
-import Paragraph from 'grommet/components/Paragraph';
-import Accordion from 'grommet/components/Accordion';
-import AccordionPanel from 'grommet/components/AccordionPanel';
 import FaBeer from 'react-icons/lib/fa/beer';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
 class App extends Component {
 
@@ -27,53 +20,13 @@ class App extends Component {
   
   render() {
     return (
-        <AppWrapper centered>
-          <Header size="large">
-          </Header>
-          <Split
-            fixed={false}
-            priority='left'
-            flex='right'
-            separator={false}
-            showOnResponsive='both'
-          >
-            <Sidebar>
-              <Accordion>
-                <AccordionPanel heading='First Title'>
-                  <Paragraph>
-                    
-                    <Button icon={<FaBeer />}
-                      label='Label'
-                      onClick={() => {}}
-                      href='#'
-                      plain={false}
-                      critical={false}
-                      accent={false}
-                      secondary={false}
-                      primary={true} 
-                    />
-                  </Paragraph>
-                </AccordionPanel>
-                <AccordionPanel heading='Second Title'>
-                  <Paragraph>
-                    <Button icon={<FaBeer />}
-                      label='Label'
-                      onClick={() => {}}
-                      href='#'
-                      plain={false}
-                      critical={false}
-                      accent={false}
-                      secondary={false}
-                      primary={true} 
-                    />
-                  </Paragraph>
-                </AccordionPanel>
-              </Accordion>
-            </Sidebar>
-            <Section>
-            </Section>
-          </Split>
-        </AppWrapper>
+        <Toolbar>
+          <ToolbarGroup firstChild={true}>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <RaisedButton label="Publish" primary={true} />
+          </ToolbarGroup>
+        </Toolbar>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'grommet-css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './containers/App';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.css';
-
 import reducers from './reducers';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 
 const store = createStore(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
